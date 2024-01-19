a839ddecc22117a90f1cba206d0e415d9b9b8588
Nikolaev1ch <vsharanov@yandex.ru>
2024-01-19 13:32:35 +0500

    "Проверка работоспособности"

---
+++ venv/icecream.py
@@ -0,0 +1,6 @@
+print('My love icecream - Plombir')
+print('You name Vitaliy')
+input('\n\nНажмите на Enter. для продолжения.')
+
+
+print('Истина — это то, что выдерживает проверку опытом (А. Эйнштейн)')
